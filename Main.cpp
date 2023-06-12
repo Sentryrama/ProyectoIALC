ccb3651359adc2703ae5b62c8da99e08fe6e529a
Harold Camacho Herrera <haroldherrera2001@outlook.com>
2023-06-12 13:59:18 -0600

    Update Main.cpp
    Eliminacion de los campos de la IP, remplazados por por un bucle for controlado

--- Main.cpp
+++ Main.cpp
@@ -15,13 +15,7 @@ enum Step
     Two,
     Three
 };
-enum Field
-{
-    First,
-    Second,
-    Third,
-    Fourth
-};
+
 bool isDigit(char key)
 {
     return key >= 48 && key <= 57;
@@ -30,17 +24,20 @@ void mostrar_direccionIP(string direccionIP)
 {
     cout << direccionIP << endl;
 }
-void mostar_field(Field field){
-    cout << "Estas en " << field << " y pasas a " << (field+1) << endl;
-}
-int automata(char key)
-{
-    static string direccionIP = "";
-    static Step step = Step::One;
-    static Field field = Field::First;
-    char eleccion;
 
-    if (isDigit(key))
+int automata(char key, string &direccionIP)
+{
+
+    static Step step = Step::One;
+    char eleccion;
+    if (key == 'b' || key == 'B')
+    {
+        if (direccionIP.length() - 1 == '.')
+            direccionIP.erase(direccionIP.length() - 1);
+        else
+            direccionIP.erase(direccionIP.length() - 2, 2);
+    }
+    else if (isDigit(key))
     {
         switch (step)
         {
@@ -49,60 +46,38 @@ int automata(char key)
             {
                 cout << "Quieres que el campo sea unicamente 0? (S/N): ";
                 cin >> eleccion;
-                if (eleccion == 'S' || eleccion == 's' )
+                if (eleccion == 'S' || eleccion == 's')
                 {
-
-                    if (field == Field::First)
-                    {
-                        direccionIP.append("0.");
-                        field = Field::Second;
-                        mostar_field(field);
-                        return 1;
-                    }
-                    else if (field == Field::Second)
-                    {
-                        direccionIP.append("0.");
-                        field = Field::Third;
-                        mostar_field(field);
-                        return 2;
-                    }
-                    else if (field == Field::Third)
-                    {
-                        direccionIP.append("0.");
-                        field = Field::Fourth;
-                        mostar_field(field);
-                        return 3;
-                    }
-                    else if (field == Field::Fourth)
-                    {
-                        direccionIP.append("0");
-                        mostar_field(field);
-                        mostrar_direccionIP(direccionIP);
-                        return 4;
-                    }
-
-                    break;
+                    direccionIP.append("0");
+                    return 3;
                 }
-                else if (key == '1')
+                else if (eleccion == 'N' || eleccion == 'n')
                 {
-                    direccionIP.append("1");
-                    // Agrega el 1 a la IP
-                    // set step = two
+                    return 0;
                 }
-                else if (key == '2')
-                {
-                    // Agrega el 2 a la IP
-                    // set step = two
-                }
-                else if (key >= '3')
-                {
-                    // Agrega el 3 a la IP
-                    // set step = two
-                    cout << "Hola" << endl;
-                }
+                else
+                    cout << "Solo puedes ingresar S o N" << endl;
             }
-            else
-                cout << "Solo puedes ingresar S o N" << endl;
+            else if (key == '1')
+            {
+                direccionIP.append("1");
+                step = Step::Two;
+                return 1;
+            }
+            else if (key == '2')
+            {
+                direccionIP.append("2");
+                step = Step::Two;
+                return 1;
+            }
+            else if (key >= '3')
+            {
+                string caracter = string(1, key);
+                step = Step::Two;
+                direccionIP.append(caracter);
+                return 1;
+            }
+
             break;
         case Step::Two:
 
@@ -119,17 +94,26 @@ int automata(char key)
 
 int main()
 {
+    static string direccionIP = "/";
     char key;
-    int i = 0, k = 0;
-    while (i < 4)
+    int k = 0;
+    cout << &direccionIP << endl;
+    for (int i = 0; i < 4; i++)
     {
-        cout << "Ingresa un digito numerico: ";
-        cin >> key;
-        i = automata(key);
-        /*while (k < 3)
+        if (i != 4 && k == 3)
         {
-        }*/
-        // i++;
+            direccionIP += ".";
+        }
+        k = 0;
+        while (k < 3)
+        {
+            cout << "Ingresa un digito numerico: ";
+            cin >> key;
+            if (key == 'b')
+                i--;
+            k = automata(key, direccionIP);
+            cout << "El valor de k: " << k << " El valor de i: " << i << endl;
+            cout << "Direccion compuesta: " << direccionIP << endl;
+        }
     }
 }
-
